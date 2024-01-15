2ada799fb432b7bacf5156b29a17ae7b845a2b8f
vdunlop <148563390+vdunlop@users.noreply.github.com>
2024-01-15 13:59:19 -0600

    Removed my extraneous files

--- indexhold.js
+++
@@ -1,8 +0,0 @@
-const userInput = require("./lib/userInput");
-
-userInput.run();
-
-// Write the formatted input to the readme file.
-fs.writeFile(svgFileName, svgText, (err) =>
-  err ? console.log(err) : console.log("SVG File Created.")
-);
